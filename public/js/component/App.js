4bd237f7a535eaf014ae4954eb0f5e2d2049abc8
unknown <collinb512@gmail.com>
2019-05-17 15:13:31 -0500

    initial upload

---
+++ public/js/component/App.js
@@ -0,0 +1,43 @@
+function App(){
+    return(
+    <body>
+    <div id="root">
+        <div class="App">
+           <Header/>
+           <div class="container">
+               <div class="row">
+                   <div class="col-md-3">
+                       <p class="lead">Shop Name</p>
+                       <div class="list-group">
+                           <a href="#" class="list-group-item">Category 1</a>
+                           <a href="#" class="list-group-item">Category 2</a>
+                           <a href="#" class="list-group-item">Category 3</a>
+                       </div>
+                   </div>
+                   <div class="col-md-9">
+                      <Carousel/>
+                       <div class="row">
+                           <ProductDetail />
+                       </div>
+       
+                   </div>
+       
+               </div>
+       
+           </div>
+          
+           <div class="container">
+       
+               <hr/>
+       
+            <Footer/>
+
+           </div>
+             </div>
+        
+       
+
+    </div>
+    </body>
+    )
+}
---
+++ public/js/component/Footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+                   
+        <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Your Website 2014</p>
+            </div>
+        </div>
+    </footer>
+    )
+}
