66a6b67ef4aa83bf22fec915a966a2e756e2cab5
daniela-idara <purpleelephant1987@gmail.com>
2019-08-14 20:35:43 -0500

    stars are working!

--- public/js/components/ProductDetail.js
+++ public/js/components/ProductDetail.js
@@ -1,21 +1,30 @@
 function ProductDetail(props){
+
+    let star = [];
+    for(let i = 0; i < props.product.rating; i ++){
+      star.push(<span key={i} className="glyphicon glyphicon-star"></span>);
+    }
+
+    let emptystars = [];
+
+    for(let i = 0; i < (5 - props.product.rating); i ++){
+        emptystars.push(<span class="glyphicon glyphicon-star-empty"></span>);
+      }
+
+
     return(<div class="col-sm-4 col-lg-4 col-md-4">
     <div class="thumbnail">
-        <img src="http://placehold.it/320x150" alt=""/>
+        <img src={props.product.imgUrl} alt=""/>
         <div class="caption">
-            <h4 class="pull-right">$24.99</h4>
-            <h4><a href="#">First Product</a>
+            <h4 class="pull-right">{props.product.price}</h4>
+            <h4><a href="#">{props.product.name}</a>
             </h4>
             <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
         </div>
         <div class="ratings">
-            <p class="pull-right">15 reviews</p>
+            <p class="pull-right">{props.product.reviews} reviews</p>
             <p>
-                <span class="glyphicon glyphicon-star"></span>
-                <span class="glyphicon glyphicon-star"></span>
-                <span class="glyphicon glyphicon-star"></span>
-                <span class="glyphicon glyphicon-star"></span>
-                <span class="glyphicon glyphicon-star"></span>
+                {star}{emptystars}
             </p>
         </div>
     </div>
