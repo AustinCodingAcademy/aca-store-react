1c1dc965f332301e1e709ef845ea468c8e13258c
alaynapuck28 <you@example.com>
2019-08-17 12:52:00 -0500

    part one

---
+++ public/js/component/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="row">
+        <div className="col-lg-12">
+          <p>Copyright &copy; Your Website 2014</p>
+        </div>
+      </div>
+    </footer>
+    // <!--</Footer>-->
+  );
+}
